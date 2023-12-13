c3d83f80a421bf70773873ccbf628b44f4dab65e
Jannek Ulm <jannek.ulm@icloud.com>
2023-12-13 10:56:33 +0100

    changes to env

---
+++ Custom_ResNet18_Training.py
@@ -0,0 +1,33 @@
+import infrastructure as inf
+
+
+from argparse import ArgumentParser
+from typing import List
+import time
+import numpy as np
+from tqdm import tqdm
+
+import torch    
+
+import torch as ch
+from torch.cuda.amp import GradScaler, autocast
+from torch.nn import CrossEntropyLoss, Conv2d, BatchNorm2d
+from torch.optim import SGD, lr_scheduler
+from torchvision.transforms import v2
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+from fastargs import get_current_config, Param, Section
+from fastargs.decorators import param
+from fastargs.validation import And, OneOf
+
+from ffcv.fields import IntField, RGBImageField
+from ffcv.fields.decoders import IntDecoder, SimpleRGBImageDecoder
+from ffcv.loader import Loader, OrderOption
+from ffcv.pipeline.operation import Operation
+from ffcv.transforms import RandomHorizontalFlip, Cutout, \
+    RandomTranslate, Convert, ToDevice, ToTensor, ToTorchImage
+from ffcv.transforms.common import Squeeze
+from ffcv.writer import DatasetWriter
